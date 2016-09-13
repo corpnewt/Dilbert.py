bdc205e7db57c07a07a1c8a7295a89490b7225cf
CorpNewt <corporatenewt@gmail.com>
2016-09-13 14:02:35 -0500

    Add files via upload

---
+++ dilbert.py
@@ -0,0 +1,205 @@
+import urllib.request
+import urllib
+import re
+import webbrowser
+import os
+import platform
+import html
+import math
+
+try:
+    # Python 2.6-2.7
+    from HTMLParser import HTMLParser
+except ImportError:
+    # Python 3
+    from html.parser import HTMLParser
+
+import datetime
+
+def julianDate(my_date):
+    # Takes a date string MM-DD-YYYY and
+    # returns Julian Date
+    date = my_date.split("-")
+
+    month = int(date[0])
+    day   = int(date[1])
+    year  = int(date[2])
+
+    month=(month-14)/12
+    year=year+4800
+    JDate=1461*(year+month)/4+367*(month-2-12*month)/12-(3*((year+month+100)/100))/4+day-32075
+    return JDate
+
+# Function from:  https://gist.github.com/jiffyclub/1294443
+def date_to_jd(year,month,day):
+    if month == 1 or month == 2:
+        yearp = year - 1
+        monthp = month + 12
+    else:
+        yearp = year
+        monthp = month
+    # this checks where we are in relation to October 15, 1582, the beginning
+    # of the Gregorian calendar.
+    if ((year < 1582) or
+        (year == 1582 and month < 10) or
+        (year == 1582 and month == 10 and day < 15)):
+        # before start of Gregorian calendar
+        B = 0
+    else:
+        # after start of Gregorian calendar
+        A = math.trunc(yearp / 100.)
+        B = 2 - A + math.trunc(A / 4.)
+    if yearp < 0:
+        C = math.trunc((365.25 * yearp) - 0.75)
+    else:
+        C = math.trunc(365.25 * yearp)
+
+    D = math.trunc(30.6001 * (monthp + 1))
+
+    jd = B + C + D + day + 1720994.5
+    return jd
+
+# Function from:  https://gist.github.com/jiffyclub/1294443
+def jd_to_date(jd):
+    jd = jd + 0.5
+    F, I = math.modf(jd)
+    I = int(I)
+    A = math.trunc((I - 1867216.25)/36524.25)
+    if I > 2299160:
+        B = I + 1 + A - math.trunc(A / 4.)
+    else:
+        B = I
+    C = B + 1524
+    D = math.trunc((C - 122.1) / 365.25)
+    E = math.trunc(365.25 * D)
+    G = math.trunc((C - E) / 30.6001)
+    day = C - E + F - math.trunc(30.6001 * G)
+    if G < 13.5:
+        month = G - 1
+    else:
+        month = G - 13
+    if month > 2.5:
+        year = D - 4716
+    else:
+        year = D - 4715
+    return year, month, day
+
+# Clear screen
+def cls():
+    if platform.system() == "Windows":
+        os.system('cls')  # for Windows
+    else:
+        os.system('clear')  # for Linux/OS X
+
+# Find string between 2 strings
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+# Download image to location
+def downloadImage( url, fileName ):
+    with urllib.request.urlopen(url) as f:
+        htmlSource = str(f.read())
+        imageURL = find_between( htmlSource, "data-image=", "data-date=" )
+        urlNoQuotes = imageURL.replace('"', '').strip()
+        #print("Image URL: " + str(urlNoQuotes))
+        #webbrowser.open(urlNoQuotes)
+        urllib.request.urlretrieve(urlNoQuotes, fileName)
+
+def downloadImageTo( url, fileName ):
+    urllib.request.urlretrieve(url, fileName)
+
+def getImageHTML ( url ):
+    with urllib.request.urlopen(url) as f:
+        htmlSource = str(f.read())
+        return htmlSource
+
+def getImageURL ( html ):
+    imageURL = find_between( html, "data-image=", "data-date=" )
+    return imageURL.replace('"', '').strip()
+
+def getImageTitle ( html ):
+    imageTitle = find_between( html, "data-title=", "data-tags=" )
+    h = HTMLParser()
+    imageTitle = h.unescape(imageTitle)
+    #print(h.unescape(imageTitle))
+    return imageTitle.replace('"', '').strip()
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Start of script                                                       #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+cls()
+response = input("Enter the start date (MM-DD-YYYY): ")
+try:
+    startDate = response.split("-")
+except ValueError:
+    print("Not valid date.")
+    exit
+
+startJDate = date_to_jd(int(startDate[2]), int(startDate[0]), int(startDate[1]))
+
+response = input("Enter the end date (MM-DD-YYY): ")
+try:
+    endDate = response.split("-")
+except ValueError:
+    print("Not valid date.")
+    exit
+
+endJDate = date_to_jd(int(endDate[2]), int(endDate[0]), int(endDate[1]))
+difference = endJDate - startJDate + 1
+
+print(str(int(difference)) + " days to download...")
+
+while (startJDate <= endJDate):
+    # Let's get the date - and parse/download our image
+    gDate = jd_to_date(startJDate)
+
+    # Prep dir names
+    yDir = str(gDate[0])
+    mDir = str(gDate[1])
+    dName = str(int(gDate[2]))
+
+    if (gDate[1] < 10):
+        mDir = "0"+mDir
+
+    if (gDate[2] < 10):
+        dName = "0"+dName
+
+    dirPath = "./Dilbert/" + yDir + "/" + mDir
+
+    # Check directories
+    if not os.path.isdir(dirPath):
+        os.makedirs(dirPath)
+
+    # Get URL
+    getURL = "http://dilbert.com/strip/" + str(gDate[0]) + "-" + mDir + "-" + dName
+
+    print("Getting source from: " + getURL)
+
+    # Retrieve HTML and other info
+    imageHTML = getImageHTML(getURL)
+    imageURL  = getImageURL(imageHTML)
+    imageName = dName + " - " + getImageTitle(imageHTML) + ".jpg"
+    imageDest = dirPath + "/" + imageName
+
+    print("Image located at: " + imageURL)
+    print("Downloading to: " + imageName)
+
+    # Check if image exists
+    if os.path.isfile(imageDest):
+        print("ERROR:  Already Exists - skipping.")
+    else:
+        # Download image
+        downloadImageTo( imageURL, imageDest)
+
+    # Increment Counter
+    startJDate += 1
+
+    # Print newline
+    print("\n")
